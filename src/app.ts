924b2807f65a521910cb446687f691dda16dbc3e
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-31 16:54:58 +0100

    update-cors
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { mobsRouter } from "./routers/mobs-router";
 import { notFoundHandler } from "./middleware/not-found-handler";
+import cors from "cors";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
