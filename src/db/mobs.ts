82963cbf7657771fbfedadf92119a2bf8d146920
CarlUdo <carl@wbsweden.com>
2024-10-31 16:23:48 +0100

    Put-db-in-own-file
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ src/db/mobs.ts
@@ -0,0 +1,64 @@
+export const mobs = [
+  {
+    name: "Kåda",
+    id: "1",
+    members: [
+      {
+        name: "Anton",
+        id: "11",
+      },
+      {
+        name: "Leo",
+        id: "12",
+      },
+      {
+        name: "Carl",
+        id: "13",
+      },
+    ],
+  },
+  {
+    name: "Spicy",
+    id: "2",
+    members: [
+      {
+        name: "Daniel",
+        id: "21",
+      },
+      {
+        name: "Victor",
+        id: "22",
+      },
+      {
+        name: "Caroline",
+        id: "23",
+      },
+      {
+        name: "Odin",
+        id: "24",
+      },
+    ],
+  },
+  {
+    name: "Infitie-loopers",
+    id: "3",
+    members: [
+      {
+        name: "Love",
+        id: "31",
+      },
+      {
+        name: "Adrienn",
+        id: "32",
+      },
+      {
+        name: "Ali",
+        id: "33",
+      },
+      {
+        name: "Mårten",
+        id: "34",
+      },
+    ],
+  },
+];
