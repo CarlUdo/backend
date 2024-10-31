d5ae8a10a8f2dcbb65312ad045a453ee579e4e88
CarlUdo <carl@wbsweden.com>
2024-10-31 15:39:52 +0100

    Get-members-from-mob
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/routers/mobs-router.ts
+++ src/routers/mobs-router.ts
@@ -7,9 +7,28 @@ const mobs = [
   {
     name: "Kåda",
     id: "1",
+    members: [
+      "Anton",
+      "Leo",
+      "Carl"
+    ]
   },
-  { name: "Spicy", id: "2" },
-  { name: "Infinågot", id: "3" },
+  { name: "Spicy", id: "2",
+    members: [
+      "Daniel",
+      "Caroline",
+      "Victor",
+      "Odin"
+    ]
+   },
+  { name: "Infinågot", id: "3",
+    members: [
+      "Love",
+      "Adrienn",
+      "Mårten",
+      "Ali"
+    ]
+   },
 ];
 
 mobsRouter.get("/", (req: Request, res: Response) => {
@@ -20,15 +39,28 @@ mobsRouter.get("/", (req: Request, res: Response) => {
 mobsRouter.post("/", (req: Request, res: Response) => {
   const { name } = req.body;
   const id = uuidv4();
-  mobs.push({ name, id });
+  mobs.push({ name, id, members: []});
 
   res.status(200).location(`/api/v1/mobs/${id}`).json(id);
 });
 
 mobsRouter.get("/:mobsId", (req: Request, res: Response) => {
   const mobId = req.params.mobsId;
-  console.log(mobId)
+
   const mob = mobs.find(mob => mob.id === mobId);
+
   res.status(200).json(mob);
 });
+
+mobsRouter.get("/api/v1/mobs/:mobId/members", (req: Request, resp: Response) => {
+  const mobId = req.params.mobsId;
+
+  const mob = mobs.find(mob => mob.id === mobId);
+
+  resp.status(200).json(mob?.members);
+});
+
+// app.post("/api/v1/mobs/:mobId/members", (req: Request, resp: Response) => {
+//   resp.status(200).json([]);
+// });
 
