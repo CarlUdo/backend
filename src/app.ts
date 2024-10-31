2a80e2459a3bdadbf6fb215e5545dd8c313ea50e
CarlUdo <carl@wbsweden.com>
2024-10-31 15:53:29 +0100

    Clean-up-file
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,3 @@ export const app = express();
 app.use(express.json());
 
 app.use("/api/v1/mobs", mobsRouter);
-
-// app.get("/api/v1/mobs/:mobId", (req: Request, resp: Response) => {
-//   resp.status(200).json([]);
-// });
-
-// app.get("/api/v1/mobs/:mobId/members", (req: Request, resp: Response) => {
-//   resp.status(200).json([]);
-// });
-
-// app.post("/api/v1/mobs/:mobId/members", (req: Request, resp: Response) => {
-//   resp.status(200).json([]);
-// });
-
-// app.post("/api/v1/mobs/:mobId/members/:memberId", (req: Request, resp: Response) => {
-//   resp.status(200).json([]);
-// });
