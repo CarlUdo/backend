f2ebc0222b0df686a4ec3d373798b947630091d4
CarlUdo <carl@wbsweden.com>
2024-10-31 16:43:27 +0100

    Use-not-found-handler
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { mobsRouter } from "./routers/mobs-router";
+import { notFoundHandler } from "./middleware/not-found-handler";
 
 export const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/mobs", mobsRouter);
+
+app.use(notFoundHandler);
